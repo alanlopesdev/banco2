e7c36341a6ea55e03e692486596044812cf3b202
alanlopesdev <alanlopes.dev@outlook.com>
2025-10-05 00:32:10 -0300

    Update schema.ts

--- src/db/schema.ts
+++ src/db/schema.ts
@@ -3,5 +3,7 @@ import { int, sqliteTable, text } from "drizzle-orm/sqlite-core";
 export const usersTable = sqliteTable("users_table", {
   cpf: text().primaryKey(),
   name: text().notNull(),
-  saldo: int().notNull(),
+  saldo: int().notNull().default(0),
+  arroz: text().notNull(),
+  senha: text().notNull()
 });
