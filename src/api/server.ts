71348209f2658926728481c9c7141c16931c3b53
alanlopesdev <alanlopes.dev@outlook.com>
2025-10-11 22:48:18 -0300

    att server v1.0

--- src/api/server.ts
+++ src/api/server.ts
@@ -71,8 +71,10 @@ app.post("/login", async (req, res) =>{
 app.post("/pix", async (req, res) =>{
   try{
     const cpf = req.body.cpf
-    const valorPix = req.body.valorPix
-    await db.update(usersTable).set({saldo:Number(valorPix)}).where(eq(usersTable.cpf, cpf))
+    const valorPix = req.body.valorpix
+    const saldoAtual = await db.select({saldo: usersTable.saldo}).from(usersTable).where(eq(usersTable.cpf, cpf))
+    const saldoTotal = Number(valorPix) + saldoAtual[0].saldo
+    await db.update(usersTable).set({saldo:saldoTotal}).where(eq(usersTable.cpf, cpf))
   }
   catch(error){
     console.log(error)
